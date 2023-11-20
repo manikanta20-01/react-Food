d8b6676ca150332fb02342e747b7f14f603c21fc
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2023-11-20 11:04:56 +0000

    logo change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+  "https://imgs.search.brave.com/HQBgAUcKB-ez-LvNIoeA-7pDbdiGsqf_OjAhScm6IWU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzcyL2Y4/LzQwLzcyZjg0MDU2/YTNlOGVjM2JkYTQz/NThiMThlNWE0NDc5/LmpwZw";
