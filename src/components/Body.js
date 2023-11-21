7dfc09e80a4cc7c3730969bbea7ac1190d1609e4
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2023-11-21 12:29:16 +0000

    useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    // json.use(cors());
+  };
 
   return (
     <div className="body">
